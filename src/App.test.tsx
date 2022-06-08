be0b80144b6271630cba13e983242b91cd7861af
eyupkureli <eyupkurelii@gmail.com>
2022-06-08 12:18:08 +0100

    querying multiple elements

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
@@ -6,14 +7,14 @@ import { getUser } from "./get-user";
 import { mocked } from "ts-jest/utils";
 //we mock our api call function here to have a faster test.
 jest.mock("./get-user");
-const mockGetUser = mocked(getUser, true); 
+const mockGetUser = mocked(getUser, true);
 //true means if we were returning instead of a promise, we were returning an object, make amock
 
 describe("When everything is OK", () => {
-  beforeEach( async () => {
+  beforeEach(async () => {
     // eslint-disable-next-line testing-library/no-render-in-setup
     render(<App />);
-    await waitFor(() => expect(mockGetUser).toHaveBeenCalled());  //when page render we waitfor api call
+    await waitFor(() => expect(mockGetUser).toHaveBeenCalled()); //when page render we waitfor api call
   });
 
   test("should render the App component without crashing", () => {
@@ -23,7 +24,7 @@ describe("When everything is OK", () => {
   });
 
   test("should select the children that is being passed to do CustomInput component", () => {
-    screen.getByText("Input:"); //implicit assertion, also we can put regular expression /Input/
+    screen.getAllByText("Input:"); //implicit assertion, also we can put regular expression /Input/
     // expect(screen.getByText("Input:")).toBeInTheDocument(); //explicit assertion
     // let error;
     // try {
@@ -35,18 +36,20 @@ describe("When everything is OK", () => {
   });
 
   test("should select the input element by its role", () => {
-    screen.getByRole("textbox");
-    expect(screen.getByRole("textbox")).toBeInTheDocument(); //we can also make try/catch for wrong situation
+    screen.getAllByRole("textbox");
+    expect(screen.getAllByRole("textbox")[0]).toBeInTheDocument(); //we can also make try/catch for wrong situation
+    expect(screen.getAllByRole("textbox")[1]).toBeInTheDocument(); //for explicit multiple element
+    expect(screen.getAllByRole("textbox").length).toEqual(2); //we have two input element, we are testing it
   });
 
   test("should select a label element by its text", () => {
     screen.getByLabelText("Input:");
-    expect(screen.getByLabelText("Input:")).toBeInTheDocument();
+    expect(screen.getByLabelText("Input:")).toBeInTheDocument(); //RTL accepts label as one, we do not need to make it multiple testing
   });
 
   test("should select a input element by its placeholder text", () => {
-    screen.getByPlaceholderText("Example");
-    expect(screen.getByPlaceholderText("Example")).toBeInTheDocument();
+    screen.getAllByPlaceholderText("Example");
+    expect(screen.getAllByPlaceholderText("Example")).toBeInTheDocument();
   });
   //queryBy.. does not throw error, just throw error if we use correct one for toBeNull
   test("should select the input element by its role with queryByRole", () => {
@@ -57,4 +60,41 @@ describe("When everything is OK", () => {
   // test("should not find the role whatever in our component, () => {
   //   expect(screen.queryByRole("whatever")).toBeNull();
   // });
+});
+
+describe("when the component fetches the user successfully", () => {
+  beforeEach(() => {
+    mockGetUser.mockClear(); //we clear all information stored in the mock
+  });
+
+  test("should call getUser once", async () => {
+    render(<App />);
+    await waitFor(() => expect(mockGetUser).toHaveBeenCalledTimes(1));
+  });
+
+  test("should render the username passed", async () => {
+    const name = 'John';  //created name
+    mockGetUser.mockImplementationOnce(() =>
+      Promise.resolve({ id: "1", name})
+    );
+    render(<App />);
+    expect(screen.queryByText(/Username/)).toBeNull();
+    expect(await screen.findByText(`Username: ${name}`)).toBeInTheDocument();
+    expect(await screen.findByText(/name/)).toBeInTheDocument();
+  });
+
+  //or
+
+  // test("should render the username passed", async () => {
+  //   const name = 'John';  //created name
+  //   mockGetUser.mockResolvedValueOnce({id: '1', name});
+  //   render(<App />);
+  //   expect(screen.queryByText(/Username/)).toBeNull();
+  //   expect(await screen.findByText(`Username: ${name}`)).toBeInTheDocument();
+  //   expect(await screen.findByText(/name/)).toBeInTheDocument();
+  // });
+
+
+
+
 });
